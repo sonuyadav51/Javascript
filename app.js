0786c22e052493d3c40f465e0fd0d1fd99eb18c8
sonuyadav51 <sonukyadav2510@gmail.com>
2022-03-12 22:47:06 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,138 @@
+// // // syntax
+// // let count = 10;
+// // // datatype
+
+// // // DATA TYPE
+
+// // // number
+// // let num = 10;
+// // let num2 = 12.5;
+
+// // // string -> sequece of character
+
+// // let user = "sonuyadav";
+// // let sigleUser = "ram kuamr";
+// // let count2 = "12";
+// // //console.log(num + 10);
+// // //console.log(typeof count2);
+
+// // // Boolean
+// // // true, false,1,0
+// // let sonu = 22;
+// // let selected = sonu;
+// // //console.log(typeof selected);
+// // // 2 > 5 =>
+// // // undefined
+
+// // let test;
+
+// // //console.log(typeof test);
+// // // Array
+
+// // let fruits = [1, 2, 3];
+
+// // //console.log(typeof fruits);
+
+// // // Object
+
+// // let obj = { key: "value" };
+// // console.log(typeof obj);
+
+// // let num3 = "";
+// // console.log(typeof num3);
+
+// // DATA TYPE
+// //1. number
+// // 2. string
+// //3. Boolean
+// //4. object
+// //5. array
+// //6. undefined
+
+// // Dynamic Data type, run time par apna data type change kar sakata hai
+// let num = 10;
+// console.log(typeof num);
+// num = "sonu yadav";
+// console.log(typeof num);
+// num = 2 > 5;
+// console.log(typeof num);
+// console.log(num);
+// /*
+// c++
+// int num;
+// num = "string";
+// */
+
+// OPERATORS -> MATH
+// let x = 557;
+// let y = 5;
+
+// console.log("addintion:", x + y);
+// console.log("substraction:", x - y);
+// console.log("multi:", x * y);
+// console.log("devision:", x / y);
+// console.log("exponetial: ", 2 ** 5);
+//console.log("modulo: ", x % 10);
+
+// Increment ++
+// Increment ++var
+// postIncrement var++
+// console.log(x++);
+// console.log(x);
+// Decrement --
+
+// asignment operator
+// =
+
+// let num = 10;
+
+// // num = num + 100;
+// // num += 100;
+// num -= 5;
+// num = num - 5;
+// num *= 7;
+// num = num * 7;
+// console.log(num);
+
+// Concatination => do string ko join karna
+
+// let firstName = "sonu";
+// let lastName = "yadav";
+
+// let fullName = firstName + " " + lastName;
+
+// console.log("Your fullName is: "+fullName);
+// COMPARISON OPERATOR
+
+// 1. == -> only value check karta hai
+
+//let t = 5 == "5";
+//console.log(t);
+
+// 2. === -> value + datatype
+//let newT = 5 === "5";
+//console.log(newT);
+
+// 3. >
+// >=
+//5 > 3 -> true
+
+// != not equal to -> true jab do equal nahi rahenge
+
+//  5 != 6 => true
+ // !==
+
+ //console.log( 5 !== "5");
+
+ let age = 17;
+
+ let permission = false;
+
+ // 15 year ke 
+
+ // if age > 10 || age < 20
+ // number is in between 10 and 20
+// and gate 
+ // true true => true
+
+ 
