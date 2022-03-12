2a22691655ecdfc002377f966077619ac0e691a1
sonuyadav51 <sonukyadav2510@gmail.com>
2022-03-13 00:18:22 +0530

    conditional statement if else switch

---
+++ app.js
@@ -0,0 +1,99 @@
+// // Condition statement
+
+// // 1. if()
+// // 2. if - else
+// // 3. if - if else - else
+
+// // syntax
+
+// // if (10 < 6) {
+// //   console.log("hey i am from if");
+// // } else {
+// //   console.log(" hey i am from else");
+// // }
+// //let num = 15;
+// // if (num == 10) {
+// //   console.log(" you are 10");
+// // } else if (num === 6) {
+// //   console.log(" dfklasjf");
+// // } else if (num === 20) {
+// //   console.log("37246dksj");
+// // } else {
+// //   console.log(" y ou are now selected");
+// // }
+// // anothewr way
+// // if (num == 10) {
+// //   console.log("10");
+// // }
+// // if (num == 15) {
+// //   console.log("15");
+// // }
+// // if (num == 13) {
+// //   console.log("kajsdhf");
+// // }
+
+// let password = document.querySelector(".password");
+// let btn = document.querySelector(".submit");
+
+// btn.addEventListener("click", function () {
+//   let value = password.value;
+//   // ye if else
+//   //   if (value === "") {
+//   //     alert("password field can not  be empty");
+//   //   } else {
+//   //     if (value.length < 6) {
+//   //       alert("passwor must be greater than 6");
+//   //     } else {
+//   //       alert(value);
+//   //     }
+//   //   }
+//   // emptyness
+//   if (value !== "") {
+//     if (value.length > 6 && value.length < 15) {
+//       alert(value);
+//     } else {
+//       alert(" must be in between 6 and 15 char.");
+//     }
+//   } else {
+//     alert(" can not empty");
+//   }
+// });
+// /*
+//  if(length){
+//    if(firstLettr){
+//        if(special character){
+//            // now you are ready to create y our acocunt
+//        }
+//    }
+//  }
+
+// */
+// let num = 15;
+
+// if (num == 12) {
+//   console.log("12");
+// } else if (num == 15) {
+//   console.log("15");
+// } else if (num == 17) {
+//   console.log("17");
+// } else {
+//   console.log("fhkdsf");
+// }
+
+// switch
+
+// syntax
+// let day = "wed";
+// switch (day) {
+//   case "mon":
+//     console.log("Monday");
+//     break;
+//   case "mon":
+//     console.log("Tuesday");
+//     break;
+//   case "wed":
+//     console.log("Wednesday");
+//     break;
+//   default:
+//     console.log("plese enter vailid shortcut for week name");
+// }
