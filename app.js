b6d7fcaa990f71102999c0a4cbb941ea13ab159f
sonuyadav51 <sonukyadav2510@gmail.com>
2022-03-14 09:55:37 +0530

    string intro

---
+++ app.js
@@ -0,0 +1,47 @@
+// STRING
+// "", '',``
+// "THIS IS ME"
+// 'THIS IS ALSO AN STRING'
+// `32432723`
+// let myString = "sonu yadav";
+// console.log(myString);
+// hey i'm sonu yadav
+// you don't know me.
+
+// let userName = "hey i'am sonu yadav";
+// let userName = ''
+// your name is "sonu yadv";
+// let userName = 'your name is "sonu yadav"';
+// let userName = 'your name is "sonu yadav"';
+
+// console.log(userName);
+// HOW CAN WE USE A VARIABLE I SIDE A STRING??
+
+// let fName = "dipak";
+// let lName = "kamar";
+
+// your fullname is sonu yadav;
+
+// let fullname = "your fullname is " + fName + " " + lName;
+// console.log(fullname);
+// you will get 200 rupees on this order;
+// let discount = 50;
+
+// let message = 'you will save ' + discount + ' rupees on this order';
+// console.log(message);
+
+/*
+  hey you are vry beutifull
+  i am shivam and i love you , 
+  i can't live withought you.
+
+*/
+// let test = "Akanksha bhabhi";
+
+// let forShivamLove = `hey you are vry beutifull
+// i am shivam ${test} and i love you ,
+// i can't ${10 + 5} live "withought" you.`;
+
+// console.log(forShivamLove);
+
+// let emptyString = "";
